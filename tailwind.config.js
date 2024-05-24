62e498dd961a523567dd03ab5a1f77ea2feb4109
Vladyslav Starodub <starodubvlad08@gmail.com>
2024-05-24 23:56:20 +0300

    feat: typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
+    screens: {
+      sm: '320px',
+      md: '640px',
+      lg: '1200px',
+      xl: '1440px',
+    },
+    colors: {
+      'primary': '#313237',
+      'secondary': '#89939A',
+      'icons': '#B4BDC3',
+      'elements': '#E2E6E9',
+      'hoverBG': '#FAFBFC',
+      'white': '#FFFFFF',
+      'green': '#27AE60',
+      'red': '#EB5757',
+    },
+    height: {
+      '22': '5.5rem'
+    },
     extend: {},
   },
   plugins: [],
